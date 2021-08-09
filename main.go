b4d2c511b496392d05e9a715fcdf126108f37c61
Erlangga Wulung <erlanggawulung@gmail.com>
2021-08-09 14:02:00 +0700

    add csv writer

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
@@ -13,6 +15,7 @@ type Site struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Link        string `json:"link"`
+	ImageLink   string `json:"imageLink"`
 }
 
 func main() {
@@ -22,19 +25,34 @@ func main() {
 		colly.AllowedDomains("webscraper.io"),
 	)
 
-	collector.OnHTML(".col-md-7", func(element *colly.HTMLElement) {
+	collector.OnHTML(".row", func(element *colly.HTMLElement) {
 		log.Println("Element: ", element)
 		siteTitle := element.ChildText("h2")
-		siteLink := element.ChildAttrs("a", "href")[0]
-		log.Println("siteLink: ", siteLink)
-		siteDescription := element.ChildText("p")
-		site := Site{
-			Title:       siteTitle,
-			Description: siteDescription,
-			Link:        siteLink,
+
+		var siteLink string
+		siteLinks := element.ChildAttrs("a", "href")
+		if len(siteLinks) > 0 {
+			siteLink = siteLinks[0]
 		}
 
-		allSites = append(allSites, site)
+		var imageLink string
+		imageLinks := element.ChildAttrs("img", "src")
+		if len(imageLinks) > 0 {
+			imageLink = imageLinks[0]
+		}
+
+		siteDescription := element.ChildText("p")
+
+		if len(siteTitle) > 0 {
+			site := Site{
+				Title:       siteTitle,
+				Description: siteDescription,
+				Link:        siteLink,
+				ImageLink:   imageLink,
+			}
+
+			allSites = append(allSites, site)
+		}
 	})
 
 	collector.OnRequest(func(request *colly.Request) {
@@ -44,6 +62,7 @@ func main() {
 	collector.Visit("https://webscraper.io/test-sites")
 
 	writeJSON(allSites)
+	writeCSV(allSites)
 }
 
 func writeJSON(data []Site) {
@@ -53,4 +72,31 @@ func writeJSON(data []Site) {
 		return
 	}
 	_ = ioutil.WriteFile("sites.json", file, 0644)
+}
+
+func writeCSV(data []Site) {
+	rows := [][]string{
+		{"Title", "Description", "Link", "ImageLink"},
+	}
+
+	for _, site := range data {
+		row := []string{site.Title, site.Description, site.Link, site.ImageLink}
+		rows = append(rows, row)
+	}
+
+	csvfile, err := os.Create("sites.csv")
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	csvwriter := csv.NewWriter(csvfile)
+
+	for _, row := range rows {
+		_ = csvwriter.Write(row)
+	}
+
+	csvwriter.Flush()
+
+	csvfile.Close()
 }
